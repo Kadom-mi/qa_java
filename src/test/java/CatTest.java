453f3a514907582ea22f3ad9258261c84cfa35cc
Anastasia Kadomskaya <kadommi@yandex.ru>
2025-10-10 00:29:24 +0300

    sprint-6 v.1.2

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -3,8 +3,8 @@ import com.example.Feline;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
@@ -16,7 +16,7 @@ public class CatTest {
 
     private Cat cat;
 
-    @Spy
+    @Mock
     private Feline feline;
 
     @Before
